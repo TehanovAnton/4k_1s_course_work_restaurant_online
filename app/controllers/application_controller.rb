af3b19957c838745122100d5ab32a0c6371594a3
Anton <tehanovanton@gmail.com>
2022-11-04 18:25:42 +0300

    add device token auth and sign in/up actions are working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
-        include DeviseTokenAuth::Concerns::SetUserByToken
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  skip_before_action :verify_authenticity_token
+
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up_params, keys: %i[name email])
+  # end
 end
