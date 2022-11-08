19ded95c3f8a7eb88a4a1ac6a5803b1fd181d3cf
Anton <tehanovanton@gmail.com>
2022-11-08 19:00:22 +0300

    Restaurant actions

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,12 @@
 class RestaurantsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_restaurant, only: %i[update destroy show]
+
+  def show    
+    return render json: @restaurant
+
+    render json: { error: 'wrong restaurant params' }
+  end
 
   def create
     authorize Restaurant
@@ -9,7 +16,32 @@ class RestaurantsController < ApplicationController
     render json: { error: 'wrong restaurant params' }
   end
 
+  def update
+    authorize Restaurant
+    response = { error: 'wrong restaurant params' }
+
+    response = @restaurant if @restaurant.update(restaurant_params)
+
+    render json: response
+  end
+
+  def destroy
+    authorize Restaurant
+    response = @restaurant
+
+    response = { error: 'wrong restaurant params' } unless @restaurant.destroy
+
+    render json: response
+  end
+
   private
+
+  def set_restaurant
+    @restaurant = Restaurant.find_by(id: params[:id])
+
+    update_auth_header
+    render json: { error: 'wrong restaurant params' } unless @restaurant
+  end
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :email, :address)
--- app/policies/restaurant_policy.rb
+++ app/policies/restaurant_policy.rb
@@ -1,6 +1,13 @@
 class RestaurantPolicy < ApplicationPolicy
   def create?
-    binding.pry
+    user.is_a? Admin
+  end
+
+  def update?
+    user.is_a? Admin
+  end
+  
+  def destroy?
     user.is_a? Admin
   end
 end
