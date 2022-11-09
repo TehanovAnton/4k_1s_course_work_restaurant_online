cb5a55918d99db6ca353b64b1e16f8a1c217a233
Anton <tehanovanton@gmail.com>
2022-11-09 15:04:43 +0300

    User actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,50 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user, only: %i[update destroy show]
 
-  def show
-    render json: User.find(params[:id])
+  def show    
+    return render json: @user
+
+    render json: { error: 'wrong user params' }
+  end
+
+  def create
+    authorize User
+
+    @user = User.create(user_params)
+    return render json: @user if @user.valid?
+
+    render json: { error: 'wrong user params' }
   end
 
   def update
-    authorize current_user
+    authorize User
+    response = { error: 'wrong user params' }
 
-    return render json: current_user if current_user.update(user_params)
+    response = @user if @user.update(user_params)
 
-    render json: { error: 'wrong params' }
+    render json: response
+  end
+
+  def destroy
+    authorize User
+    response = @user
+
+    response = { error: 'wrong user params' } unless @user.destroy
+
+    render json: response
   end
 
   private
 
+  def set_user
+    @user = User.find_by(id: params[:id])
+
+    update_auth_header
+    render json: { error: 'wrong user params' } unless @user
+  end
+
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :type)
   end
 end
--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,5 +1,13 @@
 class UserPolicy < ApplicationPolicy
+  def create?
+    user.is_a? Admin
+  end
+
   def update?
-    true
+    user.is_a? Admin || user.id == record.id
+  end
+  
+  def destroy?
+    user.is_a? Admin || user.id == record.id
   end
 end
