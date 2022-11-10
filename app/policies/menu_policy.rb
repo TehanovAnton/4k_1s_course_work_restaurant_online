3fb5026665eedbe7fc85aa8dd29b3a7e691bce12
Anton <tehanovanton@gmail.com>
2022-11-10 19:14:26 +0300

    Dish policy

--- app/policies/menu_policy.rb
+++ app/policies/menu_policy.rb
@@ -1,13 +1,13 @@
 class MenuPolicy < ApplicationPolicy
   def create?
-    user.is_a?(SuperAdmin) || record.restaurant.admins.include?(user)
+    user.is_a?(SuperAdmin) || record.admins.include?(user)
   end
 
   def update?
-    user.is_a?(SuperAdmin) || record.restaurant.admins.include?(user)
+    user.is_a?(SuperAdmin) || record.admins.include?(user)
   end
   
   def destroy?
-    user.is_a?(SuperAdmin) || record.restaurant.admins.include?(user)
+    user.is_a?(SuperAdmin) || record.admins.include?(user)
   end
 end
