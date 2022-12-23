cc159997422c042cb9df35e957186843cac78d45
anton <tehanovanton@gmail.com>
2022-12-23 10:27:23 +0300

    restaurant validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,9 +1,10 @@
 class Restaurant < ApplicationRecord
+  include Validations::RestaurantValidation
+
   has_many :tables
   has_many :menus, dependent: :destroy
   has_many :orders
   has_many :messages
-
   has_many :own_messages, as: :messageble, class_name: 'Message'
 
   has_many :restaurants_admins, dependent: :destroy
