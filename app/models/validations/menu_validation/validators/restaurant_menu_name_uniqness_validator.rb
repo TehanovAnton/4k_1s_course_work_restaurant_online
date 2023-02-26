a47f72356a9e19ba6845eacb67b79c96ebc0e9ea
anton <tehanovanton@gmail.com>
2023-02-26 20:47:44 +0300

    Common name uniqness validator

--- app/models/validations/menu_validation/validators/restaurant_menu_name_uniqness_validator.rb
+++ app/models/validations/menu_validation/validators/restaurant_menu_name_uniqness_validator.rb
@@ -1,27 +1,20 @@
 module Validations
   module MenuValidation
     module Validators
-      class RestaurantMenuNameUniqnessValidator < ActiveModel::Validator
-        MESSAGE = 'Menu with the same name already exists in restaurant.'.freeze
-
-        def validate(record)
-          @record = record
-          find_restaurant_menus_names
-
-          record.errors.add(:base, MESSAGE) if name_already_exists?
-        end
-
+      class RestaurantMenuNameUniqnessValidator < Validations::Validators::NameUniqnessValidator
         private
 
-        def find_restaurant_menus_names
-          @restaurant_menus_names = Menu.joins(:restaurant)
-                                        .where(restaurant_id: @record.restaurant_id)
-                                        .select(:name)
-                                        .pluck(:name)
+        class << self
+          def validator_message
+            'Menu with the same name already exists in restaurant.'
+          end
         end
 
-        def name_already_exists?
-          @restaurant_menus_names.include?(@record.name)
+        def find_names
+          @names = Menu.joins(:restaurant)
+                       .where(restaurant_id: @record.restaurant_id)
+                       .select(:name)
+                       .pluck(:name)
         end
       end
     end
