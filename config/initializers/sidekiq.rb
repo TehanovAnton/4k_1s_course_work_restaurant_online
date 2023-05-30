726a06918713888338e1a16332753660b1ae0b3f
anton <tehanovanton@gmail.com>
2023-05-30 17:10:56 +0300

    configs moves to application yml

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,10 +2,10 @@
 # установить через env
 Sidekiq.configure_server do |config|
   # config.redis = { url: 'redis://0.0.0.0:6379/0' }
-  config.redis = { url: 'redis://127.0.0.1:6379' }
+  config.redis = { url: ENV['REDIS_URL'] }
 end
 
 Sidekiq.configure_client do |config|
   # config.redis = { url: 'redis://0.0.0.0:6379/0' }
-  config.redis = { url: 'redis://127.0.0.1:6379' }
+  config.redis = { url: ENV['REDIS_URL'] }
 end
